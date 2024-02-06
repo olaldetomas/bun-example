bd1434c76ba8a6b6cdd7c2d4a4fd60a73f8bd4a8
Tomas Olalde <olaldetomas@gmail.com>
2024-02-06 11:17:48 -0300

    add scripts

---
+++ scripts/create-file.ts
@@ -0,0 +1,6 @@
+import { $ } from "bun";
+
+const filename = "github-response.json";
+const res = await fetch("https://api.github.com/users/octocat");
+const jsonRes = await res.json();
+await $`touch ${filename} | echo ${JSON.stringify(jsonRes)} > ${filename}`;
